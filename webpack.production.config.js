4149ffa1fc6a0ca4356972c95a87e95f128a4b16
Dorian Karter <dkarter@gmail.com>
2017-02-06 00:33:54 -0600

    Add webpack production config

---
+++ webpack.production.config.js
@@ -0,0 +1,42 @@
+var webpack = require('webpack');
+
+var devConfig = require('./webpack.config');
+
+module.exports = Object.assign({}, devConfig, {
+  devtool: false,
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      output: {
+        comments: false,
+      },
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+        dead_code: true,
+      },
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+    }),
+  ],
+});
