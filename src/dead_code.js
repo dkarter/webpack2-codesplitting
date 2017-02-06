bf5551addeffc711cc59a6d979e9bb40867002b3
Dorian Karter <dkarter@gmail.com>
2017-02-06 10:01:22 -0600

    Demo treeshaking with lodash

--- src/dead_code.js
+++ src/dead_code.js
@@ -1,7 +1,10 @@
+import { first } from 'lodash';
+
 export function droppedUnusedFunction() {
   console.log('droppedUnusedFunction');
 };
 
 export function retainedFunction() {
-  console.log('I will not be removed.');
+  const f = first([3,2,1]);
+  console.log('I will not be removed.', f);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 const VENDOR_LIBS = [
-  'faker',
+  'faker', 'lodash'
 ];
 
 module.exports = {
